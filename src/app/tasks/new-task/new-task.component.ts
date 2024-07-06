e665c80067be507773aa10ed368fb0950f5a9be3
manga409 <maxycovers@gmail.com>
2024-07-06 11:24:18 +0200

    shorter instantiation

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -11,11 +11,8 @@ import { TasksService } from '../tasks.service';
 })
 export class NewTaskComponent {
   private formEl = viewChild<ElementRef<HTMLFormElement>>('form');
-  private tasksService: TasksService;
 
-  constructor(tService: TasksService) {
-    this.tasksService = tService;
-  }
+  constructor(private tasksService: TasksService) {}
 
   onAddTask(title: string, description: string) {
     this.tasksService.addTask({ title, description });
