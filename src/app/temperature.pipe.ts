e0ac476b3d2e0b67a5f07418c81019711237226b
manga409 <maxycovers@gmail.com>
2024-07-06 10:17:11 +0200

    another custom pipe

--- src/app/temperature.pipe.ts
+++ src/app/temperature.pipe.ts
@@ -6,11 +6,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TemperaturePipe implements PipeTransform {
   transform(
-    value: string | number,
+    value: string | number | null,
     inputType: 'cel' | 'fah',
     outputType?: 'cel' | 'fah'
   ) {
     let val: number;
+
+    if (!value) {
+      return value;
+    }
 
     if (typeof value === 'string') {
       val = parseFloat(value);
@@ -35,6 +39,6 @@ export class TemperaturePipe implements PipeTransform {
       symbol = outputType === 'cel' ? '°C' : '°F';
     }
 
-    return `${outputTemp} ${symbol}`;
+    return `${outputTemp.toFixed(2)} ${symbol}`;
   }
 }
