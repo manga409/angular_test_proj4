6114e5426fcb1961f38527f8f628e8194cbb7f45
manga409 <maxycovers@gmail.com>
2024-07-06 18:23:07 +0200

    =======INIT=======

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-
+import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { TasksService } from './app/tasks/tasks.service';
-import { InjectionToken } from '@angular/core';
 
-export const TasksServiceToken = new InjectionToken<TasksService>(
-  'tasks-service-token'
-);
-
-bootstrapApplication(AppComponent, {
-  providers: [{ provide: TasksServiceToken, useClass: TasksService }],
-}).catch((err) => console.error(err));
-
-// bootstrapApplication(AppComponent, {
-//   providers: [TasksService],
-// }).catch((err) => console.error(err));
-//bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
